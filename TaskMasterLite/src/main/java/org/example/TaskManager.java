5f643eebf3b95d34097d790400900cbe1ea8a722
Emeline <emelinebrugiroux@gmail.com>
2025-05-15 15:23:32 +0200

    features/comments

--- TaskMasterLite/src/main/java/org/example/TaskManager.java
+++ TaskMasterLite/src/main/java/org/example/TaskManager.java
@@ -3,13 +3,25 @@ package org.example;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * Classe de gestion des tâches en mémoire
+ */
 public class TaskManager    {
+    /**
+     * Liste des tâches
+     */
     private List<String> tasks = new ArrayList<>();
-
+    /**
+     * Ajoute une tâche à la liste
+     * @param description : La description de la tâche
+     */
     public void addTask(String description) {
         tasks.add(description);
     }
-
+    /**
+     * Récupère toutes les tâches
+     * @return : retourne la liste des tâches
+     */
     public List<String> getAllTasks() {
         return tasks;
     }
